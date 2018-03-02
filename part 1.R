754355393da8759812a58827f02e4ef382515c3f
xuxu <xuxu@bu.edu>
2018-03-01 22:05:18 -0500

    commit

--- part 1.R
+++ part 1.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(stringr)
 
-url1 <- "http://www.ndbc.noaa.gov/download_data.php?filename=46035h1985"
+url1 <- "http://www.ndbc.noaa.gov/download_data.php?filename=46035h"
 url2 <- ".txt.gz&dir=data/historical/stdmet/"
 
 years <- c(1987:2017)
@@ -9,4 +9,3 @@ years <- c(1987:2017)
 urls <- str_c(url1, years, url2, sep = "")
 filenames <- str_c("mr", years, sep = "")
 
-##
